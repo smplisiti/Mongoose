a23f4030ce4bb963a7b9f913ad50512e62c71761
Heather <smplisiti@gmail.com>
2020-08-08 16:31:08 -1000

    Mongoose Part 3

--- models/campsite.js
+++ models/campsite.js
@@ -1,20 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const campsiteSchema = new Schema({
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const campsiteSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     description: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
+      type: String,
+      required: true,
+    },
+    comments: [commentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Campsite = mongoose.model('Campsite', campsiteSchema);
+const Campsite = mongoose.model("Campsite", campsiteSchema);
 
 module.exports = Campsite;
